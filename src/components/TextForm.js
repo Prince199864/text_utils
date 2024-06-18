f5b2c11ec33293d92fdb6d6e377a00c33a551adf
Prince199864 <princejose199864@gmail.com>
2024-06-18 13:29:21 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -35,13 +35,11 @@ function TextForm(props) {
                 <label htmlFor="exampleFormControlTextarea1" className="form-label text-center"><h2>Try Our Text Tools For Free</h2></label>
                 <textarea className="form-control" id="exampleFormControlTextarea1" rows="9" autoFocus value={text} onChange={handleOnChange} placeholder='Enter Your Text here' style={props.mode === 'light' ? { backgroundColor: '#E5E4E2', color: 'black' } : { backgroundColor: "black", color: 'white' }}></textarea>
             </div>
-            <div className="buttonBox d-flex align-items-center justify-content-around flex-wrap">
-                <button disabled={text.length === 0} type="button" className={`btn butn btn-${props.mode === 'light' ? 'success' : 'secondary'} btn-sm mx-2 my-2`} onClick={toggleUpCase}>Convert To UpperCase</button>
-                <button disabled={text.length === 0} type="button" className={`btn butn btn-${props.mode === 'light' ? 'success' : 'secondary'} btn-sm mx-2 my-2`} onClick={toggleLoCase}>Convert To LowerCase</button>
-                <button disabled={text.length === 0} type="button" className={`btn butn btn-${props.mode === 'light' ? 'success' : 'secondary'} btn-sm mx-2 my-2`} onClick={toggleExSpaces}>Remove Extra Spaces</button>
-                <button disabled={text.length === 0} type="button" className={`btn butn btn-${props.mode === 'light' ? 'success' : 'secondary'} btn-sm mx-2 my-2`} onClick={handleCopy}>Copy to Clipboard</button>
-                <button disabled={text.length === 0} type="button" className={`btn butn btn-${props.mode === 'light' ? 'success' : 'secondary'} btn-sm mx-2 my-2`} onClick={handleClear}>Clear Text</button>
-            </div>
+            <button disabled={text.length === 0} type="button" className={`btn butn btn-${props.mode === 'light' ? 'success' : 'secondary'} btn-sm mx-2 my-2`} onClick={toggleUpCase}>Convert To UpperCase</button>
+            <button disabled={text.length === 0} type="button" className={`btn butn btn-${props.mode === 'light' ? 'success' : 'secondary'} btn-sm mx-2 my-2`} onClick={toggleLoCase}>Convert To LowerCase</button>
+            <button disabled={text.length === 0} type="button" className={`btn butn btn-${props.mode === 'light' ? 'success' : 'secondary'} btn-sm mx-2 my-2`} onClick={toggleExSpaces}>Remove Extra Spaces</button>
+            <button disabled={text.length === 0} type="button" className={`btn butn btn-${props.mode === 'light' ? 'success' : 'secondary'} btn-sm mx-2 my-2`} onClick={handleCopy}>Copy to Clipboard</button>
+            <button disabled={text.length === 0} type="button" className={`btn butn btn-${props.mode === 'light' ? 'success' : 'secondary'} btn-sm mx-2 my-2`} onClick={handleClear}>Clear Text</button>
             <h3>Text Summary</h3>
             <p> {text.split(/\s+/).filter((e) => { return e.length !== 0 }).length} words and {text.split(/\s+/).join('').length} characters</p>
             <p>{0.008 * text.split(/\s+/).filter((e) => { return e.length !== 0 }).length} minutes read</p>
