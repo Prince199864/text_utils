2c053c9c2ef590c23ffdfc10800aed1f95b654d9
Prince199864 <princejose199864@gmail.com>
2024-06-18 13:01:24 +0530

    Update Navbar.js
    removed the search from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,10 +30,6 @@ function Navbar(props) {
                             </ul>
                         </li>
                     </ul>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
                     <div className={`mx-3 d-flex justify-content-evenly text-${props.mode === 'dark' ? 'light' : 'dark'}`}>
                         <i className="bi bi-brightness-high-fill"></i>
                         <div className={`form-check form-switch mx-1 text-${props.mode === 'dark' ? 'light' : 'dark'}`}>
